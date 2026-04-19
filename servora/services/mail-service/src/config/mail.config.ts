254c76292ebcba6e938e90e1c86bb835a14255c8
Lucky <sashankswain6@gmail.com>
2026-04-20 00:17:24 +0530

    Sync Communication Of services

--- servora/services/mail-service/src/config/mail.config.ts
+++ servora/services/mail-service/src/config/mail.config.ts
@@ -1,10 +1,9 @@
 import nodemailer from "nodemailer";
 
-export const tarnsporter = nodemailer.createTransport({
-
-    host:process.env.MAIL_HOST,
-    auth:{
-        user:process.env.MAIL_USER,
-        pass:process.env.MAIL_PASS,
-}})
-
+export const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.APP_PASSWORD,
+  },
+});
--- servora/services/mail-service/src/services/mail.services.ts
+++ servora/services/mail-service/src/services/mail.services.ts
@@ -1,16 +1,15 @@
-import { tarnsporter } from "../config/mail.config";
+import { transporter } from "../config/mail.config";
 import { IMailData } from "../controllers/mail.controller";
 
 
 export const sendMailService = async (data:IMailData) => {
 
-  let info =  await tarnsporter.sendMail({
-        from:data.from,
-        to:data.email,
-        subject:data.subject,
-        html:data.body,
-
-})
+  let info = await transporter.sendMail({
+    from: data.from,
+    to: data.email,
+    subject: data.subject,
+    html: data.body,
+  });
  
 return info;
 
